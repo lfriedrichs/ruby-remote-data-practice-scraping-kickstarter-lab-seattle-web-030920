a1363b4f972e0dd96d01634cdd75e0c000c98c26
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-19 18:10:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,10 @@
-# require libraries/modules here
-
-def create_project_hash
-html = File.read('fixtures/kickstarter.html')
+require "nokogiri"
+require "pry"
  
-kickstarter = Nokogiri::HTML(html)
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+ 
+create_project_hash
